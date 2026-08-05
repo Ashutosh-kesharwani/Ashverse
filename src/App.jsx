8325e239f14bcc2f4b83a20eeabfd375cd3ba6a7
Ashutosh <ashutoshkesharwani20021406@gmail.com>
2026-08-05 22:06:10 +0530

    Feat: Implement main application structure with routing and add  structure for core components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-
+import { BrowserRouter } from 'react-router-dom';
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from './components/index.js';
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        {/* Stars  */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
