d4f5e6ad6f38ed503eb623ee2b306cb8361cf770
Ashutosh <ashutoshkesharwani20021406@gmail.com>
2026-08-07 02:32:12 +0530

    feat: Update Vite configuration to enhance chunking strategy for better performance

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,41 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("three") ||
+              id.includes("@react-three/fiber") ||
+              id.includes("@react-three/drei")
+            ) {
+              return "three";
+            }
+
+            if (id.includes("framer-motion")) {
+              return "motion";
+            }
+
+            if (id.includes("@emailjs/browser")) {
+              return "email";
+            }
+
+            if (id.includes("react") || id.includes("react-dom")) {
+              return "react";
+            }
+
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
