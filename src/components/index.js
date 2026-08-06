7b2e025ab5bf4cdd7e7e1bc1253da33980b1811e
Ashutosh <ashutoshkesharwani20021406@gmail.com>
2026-08-07 01:16:51 +0530

    feat: Add Highlights component to showcase achievements, certifications, and education; update index files for integration

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,35 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import About from './About';
-import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
-import Contact from './Contact';
+import About from "./About";
+import AppToaster from "./AppToaster";
+import {
+  BallCanvas,
+  ComputersCanvas,
+  EarthCanvas,
+  StarsCanvas,
+} from "./canvas";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Highlights from "./Highlights";
+import CanvasLoader from "./Loader";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
 
 export {
-  Hero,
-  Navbar,
   About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
+  AppToaster,
+  BallCanvas,
+  CanvasLoader,
+  ComputersCanvas,
   Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
-}
+  EarthCanvas,
+  Experience,
+  Footer,
+  Hero,
+  Highlights,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+};
