08e1bbb66538dd8451ebf3bc080f764d26d050da
Ashutosh <ashutoshkesharwani20021406@gmail.com>
2026-08-07 01:16:12 +0530

    feat: Refactor Tech and Ball components to enhance rendering and structure

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
   return (
-    <div>Ball</div>
-  )
-}
+    <Float speed={0.8} rotationIntensity={0.4} floatIntensity={1.2}>
+      {/* Brighter lighting */}
+      <ambientLight intensity={0.55} />
 
-export default Ball
+      <directionalLight position={[2, 2, 2]} intensity={1.4} />
+
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+
+        <meshStandardMaterial
+          color="#fffdf7"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+          roughness={0.18}
+          metalness={0.18}
+        />
+
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
+};
+
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={null}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
